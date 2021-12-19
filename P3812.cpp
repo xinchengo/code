b6c2c4ed40aaf620baadd8e85be11a6719d4af54
xinchengo <xinchengo@gmail.com>
2021-12-19 15:54:40 +0800

    Change Implementation of P3812

--- P3812.cpp
+++ P3812.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int n;
 long long a[51], p[51], ans;
-void insert(long long x)
+void insert1(long long x)
 {
     for (int i = 50; i >= 0; i--)
         if (x >> i)
@@ -15,15 +15,43 @@ void insert(long long x)
             x = x ^ p[i];
         }
 }
+void insert2(long long x)
+{
+    for(int i=50;i>=0;i--)
+        if(x>>i)
+        {
+            if(p[i] == 0)
+            {
+                p[i] = x;
+                for(int j=i-1;j>=0;j--)
+                    if(p[j] && (p[i] >> j & 1))
+                        p[i] ^= p[j];
+                for(int j=i+1;j<=50;j++)
+                    if(p[j] >> i & 1)
+                        p[j] ^= p[i];
+            }
+        }
+}
 int main()
 {
     cin >> n;
     for (int i = 1; i <= n; i++)
         cin >> a[i];
-    for (int i = 1; i <= n; i++)
-        insert(a[i]);
-    for (int i = 50; i >= 0; i--)
-        if ((ans ^ p[i]) > ans)
+    int x = rand() % 2;
+    if(x)
+    {
+        for (int i = 1; i <= n; i++)
+            insert1(a[i]);
+        for (int i = 50; i >= 0; i--)
+            if ((ans ^ p[i]) > ans)
+                ans = ans ^ p[i];
+    }
+    else
+    {
+        for(int i=1;i<=n;i++)
+            insert2(a[i]);
+        for(int i=50;i>=0;i--)
             ans = ans ^ p[i];
+    }
     cout << ans << endl;
 }
