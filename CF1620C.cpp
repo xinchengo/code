603c8c9c30e231977ff2b5e2dfd5506e48d0a7f0
xinchengo <xinchengo@gmail.com>
2021-12-22 22:23:50 +0800

    Commit on 2021/12/22

---
+++ CF1620C.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int t, n, k;
+long long x;
+char s[2007];
+int lim[2007], ac[2007], acnt;
+int main()
+{
+	cin >> t;
+	while(t--)
+	{
+		cin >> n >> k >> x;
+		cin >> (s+1);
+		acnt=0;
+		for(int i=1;i<=n;i++)
+			if(s[i]=='*'&&s[i+1]!='*')
+			{
+				acnt++;
+				lim[acnt] = 1;
+				ac[acnt] = 0;
+				for(int j=i;s[j]=='*';j--)
+					lim[acnt]+=k;
+			}
+		int p = acnt;
+		x--;
+		while(x)
+		{
+			ac[p] = x%lim[p];
+			x /= lim[p];
+			p--;
+		}
+		p=0;
+		for(int i=1;i<=n;i++)
+		{
+			if(s[i] == 'a')
+				cout << 'a';
+			else if(s[i]=='*'&&s[i+1]!='*')
+			{
+				p++;
+				for(int j=1;j<=ac[p];j++)
+					cout << 'b';
+			}
+		}
+		cout << endl;
+	}
+}
