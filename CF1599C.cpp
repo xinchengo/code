47eb9be60b1430b092915e32c0b5bd037289b8e9
xinchengo <xinchengo@gmail.com>
2022-01-06 22:51:16 +0800

    Commit on 2022/1/6

---
+++ CF1599C.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#define c3(x) ((x)*((x)-1)*((x)-2)/6)
+#define c2(x) ((x)*((x)-1)/2)
+#define c1(x) (x)
+using namespace std;
+long long n, i;
+long double p;
+int main()
+{
+    cin >> n >> p;
+    for(i=0;1.0l*(c3(i)*2LL+c2(i)*c1(n-i)*2LL+c1(i)*c2(n-i))/(2LL*c3(n)) < p-1e-6; i++);
+    cout << i << endl;
+}
---
+++ CF1606E.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+const int mod = 998244353;
+int n, x, ans;
+int c[507][507];
+int f[507][507];
+int quick_pow(int b, int p)
+{
+    int ans = 1;
+    while (p)
+    {
+        if (p & 1)
+            ans = 1LL * ans * b % mod;
+        b = 1LL * b * b % mod;
+        p >>= 1;
+    }
+    return ans;
+}
+int calc(int i, int j)
+ {
+    if (f[i][j] != -1)
+        1;
+    else if (i > j)
+        f[i][j] = ((quick_pow(j, i) - quick_pow(j - 1, i))%mod + mod) % mod;
+    else if (i <= j)
+    {
+        f[i][j] = 0;
+        for (int k = 0; k < i; k++)
+            f[i][j] = (f[i][j] + ((1LL * calc(i - k, j - i + 1) * quick_pow(i - 1, k)) % mod) * c[i][k] % mod) % mod;
+    }
+    return f[i][j];
+}
+int main()
+{
+    cin >> n >> x;
+    c[0][0] = 1;
+    for (int i = 1; i <= 500; i++)
+        c[i][0] = 1;
+    for (int i = 1; i <= 500; i++)
+        for (int j = 1; j <= i; j++)
+            c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]) % mod;
+    for (int i = 0; i <= 500; i++)
+        for (int j = 0; j <= 500; j++)
+            f[i][j] = -1;
+    for(int i=1;i<=x;i++)
+        ans += calc(n, i), ans %= mod;
+    cout << ans << endl;
+}
