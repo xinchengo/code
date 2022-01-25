a3739f32b943540ff701a27572973c9f077a714f
Clang Format <clang-format@1337z.ninja>
2022-01-25 08:47:32 +0000

    apply clang-format

--- CF1621D.cpp
+++ CF1621D.cpp
@@ -1,21 +1,23 @@
 nclude<iostream>
-#include<algorithm>
-using namespace std;
+#include <algorithm>
+    using namespace std;
 int t, n;
 long long a[512][512], sum;
 int main()
 {
-	cin >> t;
-	while(t--)
-	{
-		cin >> n;
-		sum = 0;
-		for(int i=1;i<=n*2;i++)
-			for(int j=1;j<=n*2;j++)
-				cin >> a[i][j];
-		for(int i=n+1;i<=n*2;i++)
-			for(int j=n+1;j<=n*2;j++)
-				sum += a[i][j];
-		cout << sum + min({a[1][n+1],a[1][n*2],a[n][n+1],a[n][n*2],a[n+1][1],a[n+1][n],a[n*2][1],a[n*2][n]}) << endl;
-	}
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        sum = 0;
+        for (int i = 1; i <= n * 2; i++)
+            for (int j = 1; j <= n * 2; j++)
+                cin >> a[i][j];
+        for (int i = n + 1; i <= n * 2; i++)
+            for (int j = n + 1; j <= n * 2; j++)
+                sum += a[i][j];
+        cout << sum + min({a[1][n + 1], a[1][n * 2], a[n][n + 1], a[n][n * 2], a[n + 1][1], a[n + 1][n], a[n * 2][1],
+                           a[n * 2][n]})
+             << endl;
+    }
 }
--- CF1624F.cpp
+++ CF1624F.cpp
@@ -1,23 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int n;
-int lim=1, jmp, lval, val=0;
+int lim = 1, jmp, lval, val = 0;
 int main()
 {
-	cin >> n;
-	while(lim<n)lim<<=1;lim>>=1;
-	for(int i=lim;i>=1;i>>=1)
-	{
-		if(val == lval)
-			jmp = 0;
-		else
-			jmp = 1;
-		cout << "+ " << (jmp?(n-i):i) << endl;
-		lval = val;
-		cin >> val;
-	}
-	if(val == lval)
-		cout << "! " << (val+1)*n-1 << endl;
-	else
-		cout << "! " << (val)*n << endl;
+    cin >> n;
+    while (lim < n)
+        lim <<= 1;
+    lim >>= 1;
+    for (int i = lim; i >= 1; i >>= 1)
+    {
+        if (val == lval)
+            jmp = 0;
+        else
+            jmp = 1;
+        cout << "+ " << (jmp ? (n - i) : i) << endl;
+        lval = val;
+        cin >> val;
+    }
+    if (val == lval)
+        cout << "! " << (val + 1) * n - 1 << endl;
+    else
+        cout << "! " << (val)*n << endl;
 }
