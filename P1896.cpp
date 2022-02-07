39c539409aa653741d138103b63be9393f508f7c
Clang Format <clang-format@1337z.ninja>
2022-02-07 11:22:19 +0000

    apply clang-format

--- P1896.cpp
+++ P1896.cpp
@@ -1,51 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int n, k;
 long long f[17][100][517], ans;
 int popcnt(int x)
 {
-	int cnt = 0;
-	while(x)
-	{
-		cnt += x & 1;
-		x >>= 1;
-	}
-	return cnt;
+    int cnt = 0;
+    while (x)
+    {
+        cnt += x & 1;
+        x >>= 1;
+    }
+    return cnt;
 }
 int main()
 {
-	cin >> n >> k;
-	for(int i=0;i<(1<<n);i++)
-	{
-		if((i & (i>>1)) == 0)
-		{
-			f[1][popcnt(i)][i] = 1;
-		}
-	}
-	for(int l=2;l<=n;l++)
-	{
-		for(int i=0;i<(1<<n);i++)
-		{
-			if((i & (i>>1)) == 0)
-			{
-				for(int j=0;j<(1<<n);j++)
-				{
-					if((i & j) == 0 && ((i>>1) & j) == 0 && ((i<<1) & j) == 0) 
-					{
-						int base = popcnt(i) + popcnt(j);
-						int icnt = popcnt(i);
-						for(int c = base; c <= n * n; c++)
-						{
-							f[l][c][i] += f[l-1][c-icnt][j];
-						}
-					}
-				}	
-			}
-		}
-	}
-	for(int i=0;i<(1<<n);i++)
-	{
-		ans += f[n][k][i];
-	}
-	cout << ans << endl;
+    cin >> n >> k;
+    for (int i = 0; i < (1 << n); i++)
+    {
+        if ((i & (i >> 1)) == 0)
+        {
+            f[1][popcnt(i)][i] = 1;
+        }
+    }
+    for (int l = 2; l <= n; l++)
+    {
+        for (int i = 0; i < (1 << n); i++)
+        {
+            if ((i & (i >> 1)) == 0)
+            {
+                for (int j = 0; j < (1 << n); j++)
+                {
+                    if ((i & j) == 0 && ((i >> 1) & j) == 0 && ((i << 1) & j) == 0)
+                    {
+                        int base = popcnt(i) + popcnt(j);
+                        int icnt = popcnt(i);
+                        for (int c = base; c <= n * n; c++)
+                        {
+                            f[l][c][i] += f[l - 1][c - icnt][j];
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for (int i = 0; i < (1 << n); i++)
+    {
+        ans += f[n][k][i];
+    }
+    cout << ans << endl;
 }
--- P3622.cpp
+++ P3622.cpp
@@ -1,61 +1,61 @@
-#include<iostream>
-#include<cstring>
+#include <cstring>
+#include <iostream>
 using namespace std;
 int n, c, ans;
 int _f, _l, _t;
 struct ch
 {
-	int loc;
-	int like;
-	int fear;
-}a[50007];
+    int loc;
+    int like;
+    int fear;
+} a[50007];
 int f[10007][32];
 bool satisfy(int S, int id)
 {
-	if(S & a[id].like)
-		return 1;
-	if(~S& a[id].fear)
-		return 1;
-	return 0;
+    if (S & a[id].like)
+        return 1;
+    if (~S & a[id].fear)
+        return 1;
+    return 0;
 }
 int sat[10007][32];
 int main()
 {
-	cin >> n >> c;
-	for(int i=1;i<=c;i++)
-	{
-		cin >> a[i].loc;
-		cin >> _f >> _l;
-		for(int j=1;j<=_f;j++)
-		{
-			cin >> _t;
-			a[i].fear |= (1 << ((_t-a[i].loc+n)%n));
-		}
-		for(int j=1;j<=_l;j++)
-		{
-			cin >> _t;
-			a[i].like |= (1 << ((_t-a[i].loc+n)%n));
-		}
-		for(int S=0;S<32;S++)
-		{
-			if(satisfy(S,i))
-			{
-				sat[a[i].loc][S]++;
-			}
-		}
-	}
-	for(int S=0;S<32;S++)
-	{
-		memset(f[0], -128, sizeof(f[0]));
-		f[0][S] = 0;
-		for(int i=1;i<=n;i++)
-		{
-			for(int S=0;S<32;S++)
-			{
-				f[i][S] = max(f[i-1][(S&15)<<1], f[i-1][(S&15)<<1|1]) + sat[i][S];
-			}
-		}
-		ans = max(ans, f[n][S]);
-	}
-	cout << ans << endl;
+    cin >> n >> c;
+    for (int i = 1; i <= c; i++)
+    {
+        cin >> a[i].loc;
+        cin >> _f >> _l;
+        for (int j = 1; j <= _f; j++)
+        {
+            cin >> _t;
+            a[i].fear |= (1 << ((_t - a[i].loc + n) % n));
+        }
+        for (int j = 1; j <= _l; j++)
+        {
+            cin >> _t;
+            a[i].like |= (1 << ((_t - a[i].loc + n) % n));
+        }
+        for (int S = 0; S < 32; S++)
+        {
+            if (satisfy(S, i))
+            {
+                sat[a[i].loc][S]++;
+            }
+        }
+    }
+    for (int S = 0; S < 32; S++)
+    {
+        memset(f[0], -128, sizeof(f[0]));
+        f[0][S] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int S = 0; S < 32; S++)
+            {
+                f[i][S] = max(f[i - 1][(S & 15) << 1], f[i - 1][(S & 15) << 1 | 1]) + sat[i][S];
+            }
+        }
+        ans = max(ans, f[n][S]);
+    }
+    cout << ans << endl;
 }
