b401178351a0bb8920b03bc7d7f13eca4f20d604
xinchengo <xinchengo@gmail.com>
2022-03-22 22:45:15 +0800

    Commit on 2022/3/22

---
+++ CF570D.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+const int maxn = 5e5+7;
+int n, m;
+int head[maxn], nxt[maxn], to[maxn], ecnt;
+char val[maxn];
+void add(int u, int v)
+{
+    nxt[++ecnt] = head[u];
+    head[u] = ecnt;
+    to[ecnt] = v;
+}
+namespace q
+{
+    int head[maxn], nxt[maxn], d[maxn], id[maxn], ecnt;
+    void add(int u, int dep, int qid)
+    {
+        nxt[++ecnt] = head[u];
+        head[u] = ecnt;
+        d[ecnt] = dep;
+        id[ecnt] = qid;
+    }
+};
+bool ans[maxn];
+int fa[maxn];
+int heavy[maxn], siz[maxn], dep[maxn];
+void predfs(int u)
+{
+    dep[u] = dep[fa[u]] + 1;
+    siz[u] = 1;
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v = to[i];
+        predfs(v);
+        siz[u] += siz[v];
+        if(!heavy[u] || siz[v] > siz[heavy[u]])
+            heavy[u] = v;
+    }
+}
+bool dis[maxn][26];
+bool inc[maxn];
+void calc(int u)
+{
+    dis[dep[u]][val[u]-'a'] ^= 1;
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v = to[i];
+        if(!inc[v])
+            calc(v);
+    }
+}
+void dfs(int u, bool keep)
+{
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v = to[i];
+        if(v != heavy[u])
+            dfs(v, false), ans[u] = max(ans[u], ans[v]);
+    }
+    if(heavy[u])
+    {
+        dfs(heavy[u], true);
+        inc[heavy[u]] = true;
+    }
+    calc(u);
+    for(int i=q::head[u];i;i=q::nxt[i])
+    {
+        int cnt = 0;
+        for(int j=0;j<26;j++)
+            cnt += dis[q::d[i]][j];
+        ans[q::id[i]] = (cnt <= 1);
+    }
+    if(heavy[u])
+        inc[heavy[u]] = false;
+    if(keep == false)
+        calc(u);
+}
+int main()
+{
+    scanf( "%d%d",&n,&m);
+    for(int i=2;i<=n;i++)
+    {
+        scanf("%d",&fa[i]);
+        add(fa[i], i);
+    }
+    predfs(1);
+    scanf("%s",val+1);
+    int a, b;
+    for(int i=1;i<=m;i++)
+    {
+        scanf("%d%d",&a,&b);
+        q::add(a, b, i);
+    }
+    dfs(1, true);
+    for(int i=1;i<=m;i++)
+        printf("%s\n",(ans[i]?"Yes":"No"));
+}
---
+++ CF741D.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int maxn = 5e5+7;
+int n;
+int head[maxn], nxt[maxn], to[maxn], w[maxn], ecnt;
+void add(int u, int v, char letter)
+{
+    nxt[++ecnt] = head[u];
+    head[u] = ecnt;
+    to[ecnt] = v;
+    w[ecnt] = 1 << (letter - 'a');
+}
+int fa[maxn];
+int sta[maxn], dep[maxn], siz[maxn], heavy[maxn];
+void predfs(int u)
+{
+    dep[u] = dep[fa[u]] + 1;
+    siz[u] = 1;
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v=to[i];
+        sta[v] = sta[u] ^ w[i];
+        predfs(v);
+        siz[u] += siz[v];
+        if(!heavy[u] || siz[v] > siz[heavy[u]])
+            heavy[u] = v;
+    }
+}
+int f[1<<22], ans[maxn];
+void calc(int u, int top)
+{
+    if(f[sta[u]])
+        ans[top] = max(ans[top], dep[u] + f[sta[u]] - 2*dep[top]);
+    for(int i=0;i<22;i++)
+        if(f[sta[u]^(1<<i)])
+            ans[top] = max(ans[top], dep[u] + f[sta[u]^(1<<i)] - 2*dep[top]);
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v=to[i];
+        calc(v, top);
+    }
+}
+void add(int u)
+{
+    f[sta[u]] = max(f[sta[u]], dep[u]);
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v=to[i];
+        add(v);
+    }
+}
+void del(int u)
+{
+    f[sta[u]] = 0;
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v=to[i];
+        del(v);
+    }
+}
+void dfs(int u, bool keep)
+{
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v=to[i];
+        if(v != heavy[u])
+            dfs(v, false);
+    }
+    if(heavy[u])
+        dfs(heavy[u], true);
+    if(f[sta[u]])
+        ans[u] = max(ans[u], f[sta[u]] - dep[u]);
+    for(int i=0;i<22;i++)
+        if(f[sta[u]^(1<<i)])
+            ans[u] = max(ans[u], f[sta[u]^(1<<i)] - dep[u]);
+    f[sta[u]] = max(f[sta[u]], dep[u]);
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v=to[i];
+        if(v != heavy[u])
+        {
+            calc(v, u);
+            add(v);
+        }
+    }
+    if(keep == false)
+        del(u);
+}
+void postdfs(int u)
+{
+    for(int i=head[u];i;i=nxt[i])
+    {
+        int v=to[i];
+        postdfs(v);
+        ans[u] = max(ans[u], ans[v]);
+    }
+}
+int main()
+{
+    scanf("%d",&n);
+    char _c[10];
+    for(int i=2;i<=n;i++)
+    {
+        scanf("%d%s",&fa[i],_c);
+        add(fa[i], i, _c[0]);
+    }
+    predfs(1);
+    dfs(1,true);
+    postdfs(1);
+    for(int i=1;i<=n;i++)
+        printf("%d ",ans[i]);
+    printf("\n");
+}
