818aa23edf01048dafaac81490d930d0904dc0cc
Clang Format <clang-format@1337z.ninja>
2022-03-25 14:53:31 +0000

    apply clang-format

--- P1654.cpp
+++ P1654.cpp
@@ -1,19 +1,19 @@
-#include<iostream>
-#include<iomanip>
+#include <iomanip>
+#include <iostream>
 using namespace std;
-const int maxn = 1e5+7;
+const int maxn = 1e5 + 7;
 int n;
 double p[maxn], a[maxn], b[maxn], c[maxn];
 int main()
 {
     cin >> n;
-    for(int i=1;i<=n;i++)
+    for (int i = 1; i <= n; i++)
         cin >> p[i];
-    for(int i=1;i<=n;i++)
+    for (int i = 1; i <= n; i++)
     {
-        c[i] = (c[i-1] + 1) * p[i];
-        b[i] = (b[i-1] + 2 * c[i-1] + 1) * p[i];
-        a[i] = a[i-1] + (3 * b[i-1] + 3 * c[i-1] + 1) * p[i];
+        c[i] = (c[i - 1] + 1) * p[i];
+        b[i] = (b[i - 1] + 2 * c[i - 1] + 1) * p[i];
+        a[i] = a[i - 1] + (3 * b[i - 1] + 3 * c[i - 1] + 1) * p[i];
     }
     cout << fixed << setprecision(1) << a[n] << endl;
 }
